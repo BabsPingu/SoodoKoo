d00aa3f475538943b6cbe6bd4b420f74e973eb6d
Josh Doughty <josh.doughty01@gmail.com>
2024-05-09 16:47:30 +0100

    fully working now but wildly inefficient, need to improve

--- src/App.java
+++ src/App.java
@@ -7,16 +7,18 @@ public class App {
 
     static int[][][][] detes;
     static ArrayList<Integer>[] poss = new ArrayList[81];
-    static int c = 0;
+    static int cow = 0;
 
     public static void main(String[] args) throws Exception {
 
-        for (int i=0;i<81;i++){
-            poss[i] = new ArrayList<Integer>();
-            for(int j=1;j<=9;j++){
-                poss[i].add(j);
-            }
-        }
+
+        // new GUI();
+        run();
+
+
+    }
+
+    public static void run(){
 
         Scanner in = new Scanner(System.in);
         System.out.println("gimma dem values");
@@ -27,9 +29,27 @@ public class App {
         String ex3 = "0 0 0 0 8 7 0 1 3 0 0 0 4 0 0 0 0 0 0 0 1 2 3 0 4 0 7 0 0 5 0 0 4 0 6 2 0 0 2 5 0 9 3 0 0 1 7 0 6 0 0 5 0 0 7 0 9 0 5 1 2 0 0 0 0 0 0 0 7 0 0 0 3 1 0 9 8 0 0 0 0";
 
         //String[] base = input.split(" ");
-        String[] base = ex2.split(" ");
+        String[] base = ex3.split(" ");
 
         detes = gatherInput(base);
+
+        // for (int i=0;i<81;i++){
+        //     poss[i] = new ArrayList<Integer>();
+        //     for(int j=1;j<=9;j++){
+        //         int t = Integer.parseInt(Integer.toString(i,3));
+        //         int d = t%10; t = t/10; int c=t%10; t=t/10; int b=t%10; t=t/10; int a=t%10; t=t/10;
+        //         if(detes[a][b][c][d] == 0){
+        //             poss[i].add(j);
+        //         }
+        //     }
+        // }
+
+        for (int i=0;i<81;i++){
+            poss[i] = new ArrayList<Integer>();
+            for(int j=1;j<=9;j++){
+                poss[i].add(j);
+            }
+        }
 
         for(int i=0; i<3;i++){
             for(int j=0;j<3;j++){
@@ -44,7 +64,6 @@ public class App {
 
         System.out.println("\n");
         solve();
-
 
     }
 
@@ -66,7 +85,7 @@ public class App {
 
     public static void solve(){
         int fin = 0;
-        while(fin==0 && c<=81){
+        while(fin==0 && cow<=162){
             fin = 1;
             for(int i=0; i<3;i++){
                 for(int j=0;j<3;j++){
@@ -75,12 +94,14 @@ public class App {
                             if(detes[i][j][k][l] == 0){
                                 fin = 0;
                                 checkRelations(i,j,k,l);
+                            } else {
+                                poss[Integer.parseInt(Integer.toString(i) + Integer.toString(j) + Integer.toString(k) + Integer.toString(l),3)].clear();
                             }
                         }
                     }
                 }
             }
-            c++;
+            cow++;
         }
 
         for(int i=0; i<3;i++){
@@ -97,47 +118,79 @@ public class App {
     }
 
     public static void checkRelations(int a, int b, int c, int d){
-        ArrayList<Integer> temp = new ArrayList<>();
+        ArrayList<Integer> temp1 = new ArrayList<>();
+        ArrayList<Integer> temp2 = new ArrayList<>();
+        ArrayList<Integer> temp3 = new ArrayList<>();
 
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
-                if(detes[a][b][i][j] != 0){
-                    poss[Integer.parseInt(Integer.toString(a) + Integer.toString(b) + Integer.toString(c) + Integer.toString(d),3)].remove(Integer.valueOf(detes[a][b][i][j]));
-                } else {
-                    temp.addAll(poss[Integer.parseInt(Integer.toString(a) + Integer.toString(b) + Integer.toString(i) + Integer.toString(j),3)]);
+                if(i!=c || j!=d){
+                    if(detes[a][b][i][j] != 0){
+                        poss[Integer.parseInt(Integer.toString(a) + Integer.toString(b) + Integer.toString(c) + Integer.toString(d),3)].remove(Integer.valueOf(detes[a][b][i][j]));
+                    } else {
+                        temp1.addAll(poss[Integer.parseInt(Integer.toString(a) + Integer.toString(b) + Integer.toString(i) + Integer.toString(j),3)]);
+                    }
                 }
             }
         }
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
-                if(detes[a][i][c][j] != 0){
-                    poss[Integer.parseInt(Integer.toString(a) + Integer.toString(b) + Integer.toString(c) + Integer.toString(d),3)].remove(Integer.valueOf(detes[a][i][c][j]));
-                } else {
-                    temp.addAll(poss[Integer.parseInt(Integer.toString(a) + Integer.toString(i) + Integer.toString(c) + Integer.toString(j),3)]);
+                if(i!=b || j!=d){
+                    if(detes[a][i][c][j] != 0){
+                        poss[Integer.parseInt(Integer.toString(a) + Integer.toString(b) + Integer.toString(c) + Integer.toString(d),3)].remove(Integer.valueOf(detes[a][i][c][j]));
+                    } else {
+                        temp2.addAll(poss[Integer.parseInt(Integer.toString(a) + Integer.toString(i) + Integer.toString(c) + Integer.toString(j),3)]);
+                    }
                 }
             }
         }
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
-                if(detes[i][b][j][d] != 0){
-                    poss[Integer.parseInt(Integer.toString(a) + Integer.toString(b) + Integer.toString(c) + Integer.toString(d),3)].remove(Integer.valueOf(detes[i][b][j][d]));
-                } else {
-                    temp.addAll(poss[Integer.parseInt(Integer.toString(i) + Integer.toString(b) + Integer.toString(j) + Integer.toString(d),3)]);
+                if(i!=a || j!=c){
+                    if(detes[i][b][j][d] != 0){
+                        poss[Integer.parseInt(Integer.toString(a) + Integer.toString(b) + Integer.toString(c) + Integer.toString(d),3)].remove(Integer.valueOf(detes[i][b][j][d]));
+                    } else {
+                        temp3.addAll(poss[Integer.parseInt(Integer.toString(i) + Integer.toString(b) + Integer.toString(j) + Integer.toString(d),3)]);
+                    }
                 }
             }
         }
         if(poss[Integer.parseInt(Integer.toString(a) + Integer.toString(b) + Integer.toString(c) + Integer.toString(d),3)].size()==1){
             detes[a][b][c][d] = poss[Integer.parseInt(Integer.toString(a) + Integer.toString(b) + Integer.toString(c) + Integer.toString(d),3)].get(0);
-            c = 0;
+            cow = 0;
         }
 
         //change detes val if temp contains all possible vals from poss except 1
-        for (int v : poss[Integer.parseInt(Integer.toString(a) + Integer.toString(b) + Integer.toString(c) + Integer.toString(d),3)]){
-            temp.removeAll(Arrays.asList(v));
+
+        ArrayList<Integer> copy = new ArrayList<>();
+        copy = (ArrayList<Integer>) poss[Integer.parseInt(Integer.toString(a) + Integer.toString(b) + Integer.toString(c) + Integer.toString(d),3)].clone();
+        for (int v : temp1){
+            copy.removeAll(Arrays.asList(v));
         }
         
-        if(temp.size()==1){
-            detes[a][b][c][d] = temp.get(0);
+        if(copy.size()==1){
+            detes[a][b][c][d] = copy.get(0);
+            cow = 0;
+        }
+        
+        copy = (ArrayList<Integer>) poss[Integer.parseInt(Integer.toString(a) + Integer.toString(b) + Integer.toString(c) + Integer.toString(d),3)].clone();
+        for (int v : temp2){
+            copy.removeAll(Arrays.asList(v));
+        }
+        
+        if(copy.size()==1){
+            detes[a][b][c][d] = copy.get(0);
+            cow = 0;
+        }
+
+        copy = (ArrayList<Integer>) poss[Integer.parseInt(Integer.toString(a) + Integer.toString(b) + Integer.toString(c) + Integer.toString(d),3)].clone();
+        for (int v : temp3){
+            copy.removeAll(Arrays.asList(v));
+        }
+        
+        if(copy.size()==1){
+            detes[a][b][c][d] = copy.get(0);
+            cow = 0;
         }
 
     }

0b4f44096275413b82bf04172c8acb043aa1c413
Josh Doughty <joshua.doughty.2019@uni.strath.ac.uk>
2024-10-14 13:50:25 +0100

    updates

--- src/GUI.java
+++ src/GUI.java
@@ -6,16 +6,25 @@ import java.awt.event.ActionListener;
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class GUI implements ActionListener {
 
     public GUI(){
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
+        
+        JTextField[] grid = new JTextField[9];
+
 
         panel.setBorder(BorderFactory.createEmptyBorder(30,30,30,30));
         panel.setLayout(new GridLayout());
+        for (int i=0;i<9;i++){
+            panel.add(grid[i]);
+        }
+        
 
+        //frame.add(panel, BorderLayout.CENTER);
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("SoodoKoo");
